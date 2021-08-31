5d828248538e64d762739ac75c28bd1756f4de8f
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-31 08:43:47 -0700

    added all elements to controller gui
    early in formatting

--- src/components/Home/HeaterControl.js
+++ src/components/Home/HeaterControl.js
@@ -1,18 +1,28 @@
 import React from "react";
 import classNames from "classnames/bind";
 
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RemoveOutlinedIcon from "@material-ui/icons/RemoveOutlined";
+import AddOutlinedIcon from "@material-ui/icons/AddOutlined";
+
 import "./HeaterControl.scss";
 
 // This generates a fairly open ended button thats used in multiple places
 export default function HeaterControl(props) {
   // Generates a class tag to determine if the button is green or red
-  let heaterClass = classNames("heater-controller", {
-
-  });
+  let heaterClass = classNames("heater-controller", {});
+  const currentTemp = 31.5;
+  const targetTemp = 32;
+  const icon_styles = { fontSize: "1em" };
+  const layout_styles = { display: "flex" };
+  const zone = "Warm Hide";
 
   return (
-    <> THIS IS A HEATER CONTROL
-      </>
-
+    <div className="heater-controller">
+      <h5>{zone}</h5>
+      <AddOutlinedIcon style={icon_styles} />
+      {currentTemp} / {targetTemp}c
+      <RemoveOutlinedIcon style={icon_styles} />
+    </div>
   );
 }
