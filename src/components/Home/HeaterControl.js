0fdb0cc3437e0239638c57cf36f4fea31e23cea2
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-31 08:10:08 -0700

    hello world storybook
    got the component to display in story book

---
+++ src/components/Home/HeaterControl.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classNames from "classnames/bind";
+
+import "./HeaterControl.scss";
+
+// This generates a fairly open ended button thats used in multiple places
+export default function HeaterControl(props) {
+  // Generates a class tag to determine if the button is green or red
+  let heaterClass = classNames("heater-controller", {
+
+  });
+
+  return (
+    <> THIS IS A HEATER CONTROL
+      </>
+
+  );
+}
