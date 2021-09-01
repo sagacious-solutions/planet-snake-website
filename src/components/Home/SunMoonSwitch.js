6ef3d849119413cee13e5f6be995a1ae1d3c05b2
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-01 12:25:35 -0700

    imported missing scss

--- src/components/Home/SunMoonSwitch.js
+++ src/components/Home/SunMoonSwitch.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./SunMoonSwitch.scss";
+
 // This generates a fairly open ended button thats used in multiple places
 export default function SunMoonSwitch(props) {
   // Generates a class tag to determine if the button is green or re
