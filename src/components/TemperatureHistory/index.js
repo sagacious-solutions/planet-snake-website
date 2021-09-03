fa38f58163c976cc62a2c68808dd79dc239b5556
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-03 10:06:06 -0700

    init

---
+++ src/components/TemperatureHistory/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+// This generates a fairly open ended button thats used in multiple places
+import "./index.scss";
+
+export default function TemperatureHistory(props) {
+  return (
+    <>
+      <div class="WELLDUH">
+        <h1>TEMPERATURE HISTORY GRAPHS GO HERE</h1>
+      </div>
+    </>
+  );
+}
