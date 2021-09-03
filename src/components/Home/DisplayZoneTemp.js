7e0bd17aeb009643ede806301a854d578937f8c7
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-02 17:09:45 -0700

    Refactored to display all data and not just temperature

--- src/components/Home/DisplayZoneTemp.js
+++ src/components/Home/DisplayZoneTemp.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./DisplayZoneTemp.scss";
 // This generates a fairly open ended button thats used in multiple places
-export default function DisplayZoneTemp(props) {
+export default function DisplaySensorData(props) {
   // Generates a class tag to determine if the button is green or re
 
   return (
     <>
-      <p className="tempsOnly">
-        {props.zone} : {props.temperature}c
+      <p className="sensor-readout">
+        {props.zone} : {props.temperature}
       </p>
     </>
   );
