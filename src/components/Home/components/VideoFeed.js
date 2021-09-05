9d7b09c45ea746b3c968ca0d1c1fca0e1e91e74d
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-05 10:45:51 -0700

    removed old comments

--- src/components/Home/components/VideoFeed.js
+++ src/components/Home/components/VideoFeed.js
@@ -15,27 +15,6 @@ export default function VideoFeed(props) {
         frameborder="0"
         allowfullscreen
       ></iframe>
-      {/* <iframe
-        src="http://50.68.181.84:8411/player.html"
-        name="restreamer-player"
-        width="1280"
-        height="720"
-        scrolling="no"
-        frameborder="0"
-        webkitallowfullscreen="true"
-        mozallowfullscreen="true"
-        allowfullscreen="true"
-      ></iframe> */}
-
-      {/* <Iframe
-        width="1280"
-        height="720"
-        loading="lazy"
-        scrolling="no"
-        className="vivarium-video"
-        url="http://192.168.1.243:8081/"
-        frameBorder="0"
-      /> */}
     </>
   );
 }
