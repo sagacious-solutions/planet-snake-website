2596fb8a771a900d141b1561cfe9a310c679a51b
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-05 11:28:24 -0700

    added 3 graphs to show heater usage over course of day

--- src/components/HeaterHistory/index.js
+++ src/components/HeaterHistory/index.js
@@ -11,17 +11,33 @@ const data = {
     "Tuesday",
     "Wednesday",
     "Thursday",
-    "15h",
-    "18h",
-    "21h",
+    "Friday",
+    "Saturday",
   ],
   datasets: [
     {
-      label: "# of Votes",
-      data: [17.6, 15.8, 18.2, 24.8, 36.9, 30.1],
+      label: "% Of time Warm Hide was turned on",
+      data: [43.2, 65.5, 55.5, 73.4, 71.5, 69.5, 55.5],
       fill: false,
       backgroundColor: "rgb(255, 99, 132)",
       borderColor: "whitesmoke",
+      yAxisID: "y-axis-1",
+    },
+    {
+      label: "% Of time Basking Hide was turned on",
+      data: [99, 99, 99, 99, 99, 99, 99],
+      fill: false,
+      backgroundColor: "white",
+      borderColor: "#ff6200",
+      yAxisID: "y-axis-1",
+    },
+    {
+      label: "Room Temperature",
+      data: [30, 22, 25, 19, 18.5, 19.5, 20],
+      fill: false,
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "#03cafc",
+      yAxisID: "y-axis-2",
     },
   ],
 };
@@ -33,6 +49,13 @@ const options = {
         ticks: {
           beginAtZero: true,
         },
+        id: "y-axis-1",
+      },
+      {
+        ticks: {
+          beginAtZero: false,
+        },
+        id: "y-axis-2",
       },
     ],
   },
@@ -40,7 +63,7 @@ const options = {
 
 const LineChart = () => (
   <>
-    <Line data={data} options={options} />
+    <Line data={data} options={options} data={data} />
   </>
 );
 
