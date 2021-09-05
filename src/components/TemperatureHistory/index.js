e4be1479006a5e4b59c2eb18a278fde4d3375dac
kcruz95 <kyle.cruz@hotmail.ca>
2021-09-05 14:26:32 -0700

    graph made for TempHist

--- src/components/TemperatureHistory/index.js
+++ src/components/TemperatureHistory/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+// import { Chart } from 'chart.js/auto';
 
 // This generates a fairly open ended button thats used in multiple places
 import "./index.scss";
 
+const Chart = require('chart.js');
+
 const data = {
   labels: ['00h', '03h', '06h', '09h', '12h', '15h', '18h', '21h'],
+
   datasets: [
     {
-      label: '# of Votes',
+      label: 'Current Temperature Reading',
       data: [
         17.6,
         15.8,
@@ -17,39 +21,67 @@ const data = {
         36.9,
         30.1
       ],
-      // data: [
-      //   21.2,
-      //   16.4,
-      //   17,
-      //   22.3,
-      //   33.7,
-      //   32.4,
-      //   24.7,
-      //   21
-      // ],
       fill: false,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'whitesmoke',
+      indexAxis: 'x',
+      backgroundColor: 'skyblue',
+      borderColor: 'teal',
+      tension: 0.4,
+    },
+    {
+      label: 'Weekly Average of Current Temperature Readings',
+      data: [
+        21.2,
+        16.4,
+        17,
+        22.3,
+        33.7,
+        32.4,
+        24.7,
+        21
+      ],
+      fill: false,
+      indexAxis: 'x',
+      backgroundColor: 'orangered',
+      borderColor: 'orange',
+      tension: 0.4,
     },
   ],
 };
 
 const options = {
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true,
-        },
+  responsive: true,
+  maintainApectRatio: true,
+  color: 'gainsboro',
+
+  plugins: {
+    legend: {
+      align: 'center',
+      position: 'bottom',
+      labels: {
+
       },
-    ],
+    },
+  },
+
+  scales: {
+    y: {
+      suggestedMin: 0,
+      suggestedMax: 40,
+    },
+    // yAxes: [
+    //   {
+    //     ticks: {
+    //       beginAtZero: true,
+    //     },
+    //   },
+    // ],
   },
 };
 
 const LineChart = () => (
   <>
     <div className='header'>
-      <h1 className='title'>Line Chart</h1>
+      <h1 className='TempDisp'>Terrarium Temperature (°C)</h1>
       <div className='links'>
         {/* <a
           className='btn btn-gh'
@@ -60,6 +92,7 @@ const LineChart = () => (
       </div>
     </div>
     <Line data={data} options={options} />
+    {/* <Chart /> */}
   </>
 );
 
