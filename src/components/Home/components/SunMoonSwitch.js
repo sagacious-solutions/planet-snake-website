8a8fa603b726b46c25bd225e1ef6b909428f58e5
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-06 13:18:22 -0700

    building out the logic to spin sun moon

--- src/components/Home/components/SunMoonSwitch.js
+++ src/components/Home/components/SunMoonSwitch.js
@@ -1,18 +1,50 @@
-import React from "react";
+import { PinDropSharp } from "@material-ui/icons";
+import React, { useState } from "react";
 import "./SunMoonSwitch.scss";
 
-  const SunMoonSwitch = () => {
-    const [rotate, setRotate] = React.useState(0)
-      return (
-        <img
-          className="sun-moon-image"
-          src="images/sunmoonTrans.png"
-          alt="Sun Moon Doodad"
-          onClick={() => setRotate(1)}
-          onAnimationEnd={() => setRotate(0)}
-          rotate={rotate}
-          
-        />
-    );
-  }  
-  export default SunMoonSwitch
+const SunMoonSwitch = (props) => {
+  const [rotate, setRotate] = React.useState(0);
+  const [isDay, setIsDay] = useState(true);
+  const [wobble, setWobble] = React.useState(0);
+
+  return (
+    <img
+      src="images/sunmoonTrans.png"
+      alt="Sun Moon Doodad"
+      onClick={() => {
+        setWobble(1);
+      }}
+      // onClick={() => {
+
+      //   // props.toggleDayNight();
+      // }}
+      // `
+      className={`sun-moon-image ${isDay ? "Day" : "Night"}`}
+      onAnimationEnd={() => {
+        setIsDay(!isDay);
+        setWobble(0);
+      }}
+      wobble={wobble}
+      isDay
+    />
+  );
+};
+export default SunMoonSwitch;
+
+// import React from 'react'
+// import styles from './animated-image.module.scss'
+
+// const Image = () => {
+//   const [wobble, setWobble] = React.useState(0)
+//   return (
+//     <img
+//       className={styles.image}
+//       src="https://source.unsplash.com/random/400x200"
+//       alt="randomised!"
+//       onClick={() => setWobble(1)}
+//       onAnimationEnd={() => setWobble(0)}
+//       wobble={wobble}
+//     />
+//   )
+// }
+// export default Image
