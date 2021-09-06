4f9ad9b0accefa16037e766b1e8f9cf40d235eb0
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-06 15:30:35 -0700

    improved layout

--- src/components/SnakeHealth/index.js
+++ src/components/SnakeHealth/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 
 import SnakeHealthDataDisplay from "./SnakeHealthDataDisplay";
+import SnakeHealthButtons from "./SnakeHealthButtons";
 
 // This generates a fairly open ended button thats used in multiple places
 import "./index.scss";
@@ -8,7 +9,10 @@ import "./index.scss";
 export default function Home(props) {
   return (
     <>
-      <SnakeHealthDataDisplay />
+      <div className="pageBox">
+        <SnakeHealthButtons />
+        <SnakeHealthDataDisplay />
+      </div>
     </>
   );
 }
