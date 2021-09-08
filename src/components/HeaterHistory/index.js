7f067ea3d147b6276c4e42d4bdc912df5c4df233
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-08 10:24:33 -0700

    changed to an inversee graph

--- src/components/HeaterHistory/index.js
+++ src/components/HeaterHistory/index.js
@@ -17,53 +17,31 @@ const data = {
   datasets: [
     {
       label: "% Of Time Warm Hide Was On",
-      data: [
-        43.2,
-        65.5,
-        55.5,
-        73.4,
-        71.5,
-        69.5,
-        55.5
-      ],
+      data: [43.2, 65.5, 55.5, 73.4, 71.5, 69.5, 55.5],
       fill: false,
-      indexAxis: 'x',
-      backgroundColor: '#FFD43B',
-      borderColor: '#306998',
+      // indexAxis: "x",
+      backgroundColor: "#FFD43B",
+      borderColor: "#306998",
       tension: 0.4,
+      yAxisID: "y-axis-1",
     },
     {
       label: "% Of Time Basking Hide Was On",
-      data: [
-        99,
-        99,
-        99,
-        99,
-        99,
-        99,
-        99
-      ],
+      data: [99, 99, 99, 99, 99, 99, 99],
       fill: false,
-      backgroundColor: '#FFE873',
-      borderColor: '#4B8BBE',
+      backgroundColor: "#FFE873",
+      borderColor: "#4B8BBE",
+      yAxisID: "y-axis-1",
     },
     {
       label: "Room Temperature (External)",
-      data: [
-        30,
-        22,
-        25,
-        19,
-        18.5,
-        19.5,
-        20,
-        24
-      ],
+      data: [30, 22, 25, 19, 18.5, 19.5, 20, 24],
       fill: false,
-      indexAxis: 'x',
-      backgroundColor: '#646464',
-      borderColor: '#FFD43B',
+      // indexAxis: "x",
+      backgroundColor: "#646464",
+      borderColor: "#FFD43B",
       tension: 0.4,
+      yAxisID: "y-axis-2",
     },
   ],
 };
@@ -71,15 +49,13 @@ const data = {
 const options = {
   responsive: true,
   maintainApectRatio: true,
-  color: '#2a4d61',
+  color: "#2a4d61",
 
   plugins: {
     legend: {
-      align: 'center',
-      position: 'bottom',
-      labels: {
-
-      },
+      align: "center",
+      position: "bottom",
+      labels: {},
     },
   },
 
@@ -93,8 +69,8 @@ const options = {
 
 const LineChart = () => (
   <>
-    <div className='header'>
-      <h1 className='TempDisp'>Heat Lamp Readings (%)</h1>
+    <div className="header">
+      <h1 className="TempDisp">Heat Lamp Readings (%)</h1>
     </div>
     <Line data={data} options={options} data={data} />
   </>
