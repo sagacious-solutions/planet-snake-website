695e327121d8194529908b57b1c689b5f021eb8f
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-16 10:46:27 -0700

    partially complete displaying last snake state

--- src/components/SnakeState/Components/DataDisplay.js
+++ src/components/SnakeState/Components/DataDisplay.js
@@ -1,42 +1,48 @@
 import * as React from "react";
+import { useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import { format } from "timeago.js";
+import useDatabaseAPI from "../../../hooks/useDatabaseAPI";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, "firstName") || ""} ${
-        params.getValue(params.id, "lastName") || ""
-      }`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
-
+// This react table takes in arrays of time stamps and formats them with timeago's format function
 export default function DataDisplay() {
+  const { state, updateAll } = useDatabaseAPI();
+
+  useEffect(() => updateAll(), []);
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "event", headerName: "Event", width: 200 },
+    { field: "mostRecent", headerName: "Most Recent", width: 200 },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      event: "Poop Found",
+      mostRecent: format(state.poops_found[0]),
+    },
+    {
+      id: 2,
+      event: "Urate Found",
+      mostRecent: format(state.urates_found[0]),
+    },
+    {
+      id: 3,
+      event: "Rat Offered",
+      mostRecent: format(state.rats_offered[0]),
+    },
+    {
+      id: 4,
+      event: "Rat Ate",
+      mostRecent: format(state.rats_ate[0]),
+    },
+    {
+      id: 5,
+      event: "Rat Ignored",
+      mostRecent: format(state.rats_ignored[0]),
+    },
+  ];
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
