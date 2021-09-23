de4c3bb6d43d1f2cc4e0d4cbbf5124d83aeefb45
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-23 07:13:19 -0700

    create seperate scripts for build production and start dev

---
+++ start-dev-server.sh
@@ -0,0 +1,26 @@
+
+# Stops the website from running.
+# updates the site from git hub
+# puts together a production build
+# starts the build as a pm2 instance
+# saves pm2 so the system will reload current instances on reboot
+
+pm2 stop "planet-snake-website"
+pm2 delete "planet-snake-website"
+pm2 start "yarn start" --name "planet-snake-website"
+pm2 save
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
