effbaea7b06544bfa8c3b8ea3514e8370637f457
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-23 17:19:51 -0700

    updated to add log flush

--- start-dev-server.sh
+++ start-dev-server.sh
@@ -5,22 +5,9 @@
 # starts the build as a pm2 instance
 # saves pm2 so the system will reload current instances on reboot
 
+pm2 flush
 pm2 stop "planet-snake-website"
 pm2 delete "planet-snake-website"
 pm2 start "yarn start" --name "planet-snake-website"
 pm2 save
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+pm2 logs --raw
