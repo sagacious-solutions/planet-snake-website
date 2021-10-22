010e3ef1700d69b62a7442c96a2312b5a7a7c84f
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-22 10:07:37 -0700

    removed unused

--- src/pages/SnakeState/index.js
+++ src/pages/SnakeState/index.js
@@ -6,8 +6,10 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+
 import LatestDataDisplay from "./Components/LatestDataDisplay";
 import UpdateSnakeState from "./Components/UpdateSnakeState";
+import useDatabaseAPI from "../../hooks/useDatabaseAPI";
 
 import "./index.scss";
 
@@ -58,6 +60,8 @@ export default function ScrollableTabsButtonAuto() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
+  const { state, updateAll } = useDatabaseAPI();
+
   const handleChange = (_event, newValue) => {
     setValue(newValue);
   };
@@ -75,37 +79,17 @@ export default function ScrollableTabsButtonAuto() {
           aria-label="scrollable auto tabs example"
         >
           <Tab label="Latest" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-          <Tab label="Item Four" {...a11yProps(3)} />
-          <Tab label="Item Five" {...a11yProps(4)} />
-          <Tab label="Item Six" {...a11yProps(5)} />
-          <Tab label="Item Seven" {...a11yProps(6)} />
+          <Tab label="Last Year" {...a11yProps(1)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
         <div className="snake-state-page">
-          <LatestDataDisplay />
-          <UpdateSnakeState />
+          <LatestDataDisplay state={state} updateAll={updateAll} />
+          <UpdateSnakeState updateAll={updateAll} />
         </div>
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
       </TabPanel>
     </div>
   );
